a78f5a444fa55ee01266302186040c393fedb19b
Ben Mc <benrjmcarthur@gmail.com>
2025-03-12 00:00:19 +0000

    add(navbar): implement bottom navigation bar for improved navigation tweak(auth): wrap Stack in a View for layout consistency

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,26 @@
+import { View, StyleSheet } from "react-native";
 import { Stack } from "expo-router";
 import NavBar from "@/components/navbar";
 
 export default function AuthLayout() {
     return(
-        <Stack 
-            screenOptions={{headerShown: false}}
-            initialRouteName="index"
-        >
-            <Stack.Screen name="index" options={{title: "Welcome"}} />
-            <Stack.Screen name="login" options={{title: "Log in"}} />
-            <Stack.Screen name="register" options={{title: "Create account"}} />
-            <Stack.Screen name="forgot" options={{title: "Forgot Password"}} />
-        </Stack>
+        <View style={styles.container}>
+            <Stack 
+                screenOptions={{headerShown: false}}
+                initialRouteName="index"
+            >
+                <Stack.Screen name="index" options={{title: "Welcome"}} />
+                <Stack.Screen name="login" options={{title: "Log in"}} />
+                <Stack.Screen name="register" options={{title: "Create account"}} />
+                <Stack.Screen name="forgot" options={{title: "Forgot Password"}} />
+            </Stack>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'relative',
+    }
+})
---
+++ components/navbar.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter, usePathname } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+const NavBar = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const isActive = (path: string) => pathname === path;
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.navContent}>
+                <TouchableOpacity
+                    style={styles.navItem}
+                    onPress={() => router.navigate('/')}
+                >
+                    <Ionicons
+                        name="home-outline"
+                        size={30}
+                    />
+                    <Text style={styles.navText}>Home</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.navItem}
+                    onPress={() => {}}
+                >
+                    <Ionicons
+                        name="calendar-outline"
+                        size={30}
+                    />
+                    <Text style={styles.navText}>Schedule</Text>
+                </TouchableOpacity>
+                <View style={styles.trackCircle}>
+                    <TouchableOpacity
+                        style={styles.trackButton}
+                        onPress={() => { }}
+                    >
+                        <Ionicons name="add" size={40} color="#000" />
+                        <Text style={styles.navText}>Track</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity
+                    style={styles.navItem}
+                    onPress={() => { }}
+                >
+                    <Ionicons
+                        name="document-text-outline"
+                        size={30}
+                    />
+                    <Text style={styles.navText}>Reports</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.navItem}
+                    onPress={() => { }}
+                >
+                    <Ionicons
+                        name="person-outline"
+                        size={30}
+                    />
+                    <Text style={styles.navText}>Account</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#fff',
+    },
+    navContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        paddingHorizontal: 10,
+        backgroundColor: '#F47A60',
+        height: 70,
+        paddingVertical: 10,
+    },
+    navItem: {
+        alignItems: 'center',
+        flex: 1,
+    },
+    navText: {
+        fontSize: 15,
+        color: '#000',
+    },
+    trackCircle: {
+        backgroundColor: '#F47A60',
+        borderRadius: 30,
+        width: 60,
+        height: 60,
+        bottom: 30,
+    },
+    trackButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+});
+
+export default NavBar;
