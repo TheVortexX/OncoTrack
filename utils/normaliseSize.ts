ed0fb481ced3f50b8731204f23672f221a855e3b
Ben Mc <benrjmcarthur@gmail.com>
2025-04-20 16:06:32 +0100

    fix(normaliseSize): remove console log for width and height to clean up output

--- utils/normaliseSize.ts
+++ utils/normaliseSize.ts
@@ -1,7 +1,6 @@
 import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
-console.log('width', width, 'height', height);
 const scale = width / 430;
 
 export const normaliseSize = (size: number) => {
